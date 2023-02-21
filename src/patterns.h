f99192ace655294ccbe2e6bc1d843bbb96c463e8
Johnny <125358378+johnwickermann33@users.noreply.github.com>
2023-02-21 11:16:47 +0100

    Create patterns.h

---
+++ src/patterns.h
@@ -0,0 +1,12 @@
+#ifndef MPARK_PATTERNS_HPP
+#define MPARK_PATTERNS_HPP
+
+#include "patterns/match.hpp"
+
+#include "patterns/anyof.hpp"
+#include "patterns/as.hpp"
+#include "patterns/let.hpp"
+#include "patterns/optional.hpp"
+#include "patterns/regex.hpp"
+#include "patterns/vis.hpp"
+#include "patterns/when.hpp"
